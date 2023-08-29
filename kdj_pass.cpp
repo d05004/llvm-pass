17a7207455465e7390bd770cee23b282138d1440
d05004 <ded05004@naver.com>
2023-08-28 18:34:46 -0700

    pass v3.0 comment

--- kdj_pass.cpp
+++ kdj_pass.cpp
@@ -31,6 +31,7 @@ namespace{
         static char ID;
         KDJPass():ModulePass(ID){}
 
+        //Value to String
         string v2String(Value *value){
             string str;
             llvm::raw_string_ostream rso(str);
@@ -39,6 +40,7 @@ namespace{
             return str;
         }
 
+        //Instruction to String
         string i2String(Instruction *inst){
             string str;
             llvm::raw_string_ostream rso(str);
@@ -47,6 +49,7 @@ namespace{
             return str;
         }
         
+        //Split string by " "
         vector<string> split(string str){
             vector <string> v;
             stringstream ss(str);
@@ -56,24 +59,24 @@ namespace{
         }
 
         bool runOnModule(Module &M) override{
-
             vector<Node> master;
             vector<Node>::iterator it;
             vector<string>::iterator it2;
             unordered_set<string> blacklisted_functions={"gets","__isoc99_scanf","strcpy","sprintf"};
-            
+
+            //check stack bufferoverflow
             for (Function &F : M) {
                 for(BasicBlock &BB : F){
                     for(Instruction &I : reverse(BB)){
-                        if(isa<GetElementPtrInst>(I)){ //create node
-                            // errs()<<I<<"\n";
+
+                        //if I is GEP instruction, get length of array.
+                        if(isa<GetElementPtrInst>(I)){
                             string str=i2String(&I);
                             str=regex_replace(str,regex("[\\[\\],]"),"");
                             vector <string> v= split(str);
                             string addr = v[0];
                             string ssize=v[4];
                             size_t buf_len=stoi(ssize);
-                            // cout<<addr<<"\n";
 
                             for(it=master.begin();it!=master.end();it++){
                                 auto check=find(it->Trace_buf.begin(),it->Trace_buf.end(),addr);
@@ -81,17 +84,10 @@ namespace{
                                     it->buf_len=buf_len;
                                 }
                             }
-                            // auto it = find(Trace_buf.begin(),Trace_buf.end(),addr);
-                            // if (it==Trace_buf.end()){
-
-                            // }else{ //check overflow
-                            //     cout<<"Buf Size: "<<buf_len<<"\n";
-                            // }
                         }
-                        else if (isa<LoadInst>(I)){ //connect nodd
+                        //if I is Load Instruction, trace variable.
+                        else if (isa<LoadInst>(I)){
                             string str=i2String(&I);
-                            // errs()<<I<<"\n";
-
                             str=regex_replace(str,regex("[,]"),"");
                             vector <string> v=split(str);
 
@@ -104,29 +100,15 @@ namespace{
                                     it->Trace_buf.push_back(src);
                                 }
                             }
-
-                            // auto it = find(Trace_buf.begin(),Trace_buf.end(),dest);
-                            // if (it==Trace_buf.end()){
-
-                            // }
-                            // else{
-                            //     Trace_buf.push_back(src);
-                            // }
-
                             for(it=master.begin();it!=master.end();it++){
                                 auto check=find(it->Trace_count.begin(),it->Trace_count.end(),dest);
                                 if (check!=it->Trace_count.end()){
                                     it->Trace_count.push_back(src);
                                 }
                             }
-                            // auto it2 = find(Trace_data.begin(),Trace_data.end(),dest);
-                            // if(it2==Trace_data.end()){
-
-                            // }else{
-                            //     Trace_data.push_back(src);
-                            // }
                         }
-                        else if (isa<StoreInst>(I)){ //connect node
+                        //if I is a Store Instruction, trace variable.
+                        else if (isa<StoreInst>(I)){
                             Value *val=I.getOperand(0);
                             string str=i2String(&I);
 
@@ -142,13 +124,6 @@ namespace{
                                     it->Trace_buf.push_back(src);
                                 }
                             }
-                            // auto it = find(Trace_buf.begin(),Trace_buf.end(),dest);
-                            // if (it==Trace_buf.end()){
-
-                            // }
-                            // else{
-                            //     Trace_buf.push_back(src);
-                            // }
 
                             for(it=master.begin();it!=master.end();it++){
                                 auto check=find(it->Trace_count.begin(),it->Trace_count.end(),dest);
@@ -160,34 +135,16 @@ namespace{
                                     }
                                 }
                             }
-                            // auto it2= find(Trace_data.begin(),Trace_data.end(),dest);
-                            // if (it2==Trace_data.end()){
-                            // }else{
-                            //     if (ConstantInt *CInt=dyn_cast<ConstantInt>(val)){
-                            //         size=CInt->getSExtValue();
-                            //         errs()<<"Read Size: "<<size<<"\n";
-                            //     }else{
-                            //         Trace_data.push_back(src);
-                            //     }
-                            // }
                         }
+                        //if "read" called, start tracing buf and count.
                         else if (isa<CallInst>(I)){
-                            // errs()<<I<<"\n";
                             if (CallInst *CI = dyn_cast<CallInst>(&I)){
-
                                 Function *calledFunction=CI->getCalledFunction();
                                 if (calledFunction->getName().str()=="read"){
-                                    // call devel's function
-                                    // IRBuilder<> IRB;
-                                    // IRB.createCall();
-                                    
-
                                     Value *buf = CI->getArgOperand(1);
-                                    // errs()<<*buf<<"\n";
                                     string str=v2String(buf);
                                     vector<string> v=split(str);
                                     string buf_name=v[0];
-                                    // errs()<<"buf_name: "<<buf_name<<"\n";
 
                                     Node n;
                                     n.Trace_buf.push_back(buf_name);
@@ -195,7 +152,6 @@ namespace{
                                     Value *sizeArg=CI->getArgOperand(2);
                                     if (ConstantInt *CInt=dyn_cast<ConstantInt>(sizeArg)){
                                         size_t size=CInt->getSExtValue();
-                                        // errs()<<"Read Size: "<<size<<"\n";
                                         n.count_len=size;
                                     }else{
                                         Value *op = CI->getOperand(2);
@@ -207,16 +163,14 @@ namespace{
                                     master.push_back(n);
                                 }
                             }
-                            
                         }
                     }
                 }
             }
 
-
             //dump master
-            
             for(it=master.begin();it!=master.end();it++){
+                // //dump trace
                 // cout<<"Trace Buf"<<"\n";
                 // for(it2=it->Trace_buf.begin();it2!=it->Trace_buf.end();it2++){
                 //     cout<<*it2<<" ";
@@ -227,10 +181,14 @@ namespace{
                 //     cout<<*it2<<" ";
                 // }
                 // cout<<endl;
+
+                // // check Stack BOF
                 if (it->buf_len<it->count_len){
                     cout<<"[*] Stack Buffer Overflow Detect!!!\n";
                     cout<<"Read "<<it->count_len<<" bytes in "<<it->buf_len<<"-byte buffer"<<"\n";
                 }
+                
+                // //dump buf_len and count_len
                 // cout<<"Buf Length: "<<it->buf_len<<"\n";
                 // cout<<"Count Length: "<<it->count_len<<"\n";
             }
